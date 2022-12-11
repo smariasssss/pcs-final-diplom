19deb84b137847e62adb09946ca11fc6ecc65dce
Maria Sanzhanova <smo_0207@mail.ru>
2022-12-11 10:06:02 +0500

    Without mistakes

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -2,14 +2,13 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
     protected Map<String, List<PageEntry>> answer = new HashMap<>();
+    protected Set<String> stopWord = new HashSet<>();
 
     public BooleanSearchEngine(File pdfs) throws IOException {
 
@@ -31,12 +30,23 @@ public class BooleanSearchEngine implements SearchEngine {
                     }
 
                     for (Map.Entry<String, Integer> pair : freqs.entrySet()) {
-                        var word = pair.getKey();
-                        var count = pair.getValue();
-                        List<PageEntry> list = answer.getOrDefault(word, new ArrayList<>());
-                        list.add(new PageEntry(pdf.getName(), i, count));
-                        list.sort(Collections.reverseOrder());
-                        answer.put(word, list);
+                        List<PageEntry> pageEntries = new ArrayList<>();
+                        if (!answer.containsKey(pair.getKey())) {
+                            pageEntries.add(new PageEntry(pdf.getName(), i, pair.getValue()));
+                            answer.put(pair.getKey(), pageEntries);
+                        } else {
+                            pageEntries = answer.get(pair.getKey());
+                            pageEntries.add(new PageEntry(pdf.getName(), i, pair.getValue()));
+                            answer.put(pair.getKey(), pageEntries);
+                            Collections.sort(answer.get(pair.getKey()), PageEntry::compareTo);
+                        }
+//                        !!!исправление!!!
+//                        var word = pair.getKey();
+//                        var count = pair.getValue();
+//                        List<PageEntry> list = answer.getOrDefault(word, new ArrayList<>());
+//                        list.add(new PageEntry(pdf.getName(), i, count));
+//                        list.sort(Collections.reverseOrder());
+//                        answer.put(word, list);
                     }
                 }
             }
@@ -45,51 +55,54 @@ public class BooleanSearchEngine implements SearchEngine {
 
     @Override
     public List<PageEntry> search(String word) {
-        List<PageEntry> listAnswer = new ArrayList<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("stop-ru.txt"))) {
-            String string;
-            List<String> stopWords = new ArrayList<>();
-            while ((string = reader.readLine()) != null) {
-                stopWords.add(string);
+        List<PageEntry> listNotFirstWord = new ArrayList<>();
+        List<PageEntry> allWord = new ArrayList<>();
+
+        String[] wordMassive = word.toLowerCase().split("\\P{IsAlphabetic}+");
+        List<String> wordsSearch = new ArrayList<>();
+        for (String wordSearch : wordMassive) {
+            if (!stopWord.contains(wordSearch)) {
+                wordsSearch.add(wordSearch);
             }
-
-            String[] words = word.toLowerCase().split("\\P{IsAlphabetic}+");
-            List<String> newWords = new ArrayList<>();
-            for (String s : words) {
-                if (!stopWords.contains(s)) {
-                    newWords.add(s);
-                }
-            }
-
-            listAnswer.addAll(answer.get(newWords.get(0)));
-            for (int i = 1; i < newWords.size(); i++) {
-                List<PageEntry> listWordTwo = new ArrayList<>(answer.get(newWords.get(i)));
-
-                for (PageEntry pageWordTwo : listWordTwo) {
-                    int counter = 0;
-
-                    for (PageEntry pageAnswer : listAnswer) {
-                        if (pageAnswer.samePage(pageWordTwo)) {
-                            String pdfName = pageAnswer.getPdfName();
-                            int page = pageAnswer.getPage();
-                            int count = pageAnswer.getCount() + pageWordTwo.getCount();
-                            listAnswer.remove(pageAnswer);
-                            listAnswer.add(new PageEntry(pdfName, page, count));
-                            counter++;
-                            break;
-                        }
-                    }
-                    if (counter == 0) {
-                        listAnswer.add(pageWordTwo);
-                    }
-                }
-            }
-            listAnswer.sort(Collections.reverseOrder());
-
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
         }
-        return listAnswer;
+
+        for (String oneWordSearch : wordsSearch) {
+            allWord.addAll(answer.get(oneWordSearch));
+            break;
+        }
+
+        int value = -1;
+        for (String oneWordSearch : wordsSearch) {
+            if (value == -1) {
+                listNotFirstWord = answer.get(oneWordSearch);
+                listNotFirstWord.clear();
+                value++;
+                continue;
+            }
+            listNotFirstWord = answer.get(oneWordSearch);
+            for (PageEntry o : listNotFirstWord) {
+                value = 0;
+                for (PageEntry o1 : allWord) {
+                    if (o.getPdfName().equals(o1.getPdfName()) && o.getPage() == o1.getPage()) {
+                        allWord.remove(o1);
+                        allWord.add(new PageEntry(o1.getPdfName(), o1.getPage(), (o.getCount() + o1.getCount())));
+                        value++;
+                        break;
+                    }
+                }
+                if (value == 0) {
+                    allWord.add(o);
+                }
+            }
+        }
+
+        allWord.sort(PageEntry::compareTo);
+
+        if (allWord != null) {
+            return allWord;
+        } else {
+            return Collections.emptyList();
+        }
     }
 }
